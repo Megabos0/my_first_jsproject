7280b9b61b81aec6894e2fca1e5d9e284d2a2013
Megabos0 <61976425+Megabos0@users.noreply.github.com>
2021-03-25 22:55:36 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function day(){
+ document.getElementById('image').src='images/Morning.jpg'
+};
+
+function afternoon(){
+ document.getElementById('image').src='images/AfterNoon.jpg'
+};
+
+function evening(){
+ document.getElementById('image').src='images/Evening.jpg'
+};
+
+function night(){
+ document.getElementById('image').src='images/Night.jpg'
+};
